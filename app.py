b8162da6afa155790fa0c7714f40c2c7ab2c9fd1
GitBrianna <briannachannnel@gmail.com>
2026-07-02 00:07:28 -0400

    project reviewed

--- app.py
+++ app.py
@@ -8,25 +8,41 @@ import plotly.express as px
 
 load_data = pd.read_csv('vehicles_us.csv')
 
+st.header('How long were vehicles listed before vehicle was sold?')
 
+ls_days = px.histogram(load_data, x='days_listed', title='Duration Of Vehicles Listed', nbins=50)
+ls_days.update_layout(xaxis_title='Number Of Days Listed', yaxis_title='Number Of Vehicles')
+st.plotly_chart(ls_days)
 
-mil_vs_price = px.scatter(load_data, x='odometer', y='price', title='Price VS Milage')
+st.header('Does vehicle year affect price?')
 
-mil_vs_price.update_layout(xaxis_title='Vehicle Milage', yaxis_title='Vehicle Price')
+year_vs_price = px.scatter(load_data, x='model_year', y='price', title='Price VS Model Year')
+year_vs_price.update_layout(xaxis_title='Vehicle Year', yaxis_title='Vehicle Price')
+st.plotly_chart(year_vs_price)
+
+st.header('Does vehicle mileage affect price?')
+
+mil_vs_price = px.scatter(load_data, x='odometer', y='price', title='Price VS Mileage')
+
+mil_vs_price.update_layout(xaxis_title='Vehicle Mileage', yaxis_title='Vehicle Price')
 
 st.plotly_chart(mil_vs_price)
 
 
+st.header('Select a vehicle type')
 
-v_type = px.histogram(load_data, x='type', title='Count Of Vehicle Types')
+vehicle_type = st.selectbox(
+    "Choose a vehicle type:", 
+    sorted(load_data['type'].unique()))
 
-v_type.update_layout(
+filtered_vtype = load_data[load_data['type'] == vehicle_type]
 
-    xaxis_title='Vehicle Type',
+st.header("Condition distribution for selected vehicle type")
 
-    yaxis_title='Number of listings')
+cond_chart = px.histogram(
+    filtered_vtype,
+    x="condition",
+    title=f"Condition distribution for {vehicle_type}"
+)
 
-show_chart = st.checkbox('Show Histogram')
-
-if show_chart:
-    st.plotly_chart(v_type)
+st.plotly_chart(cond_chart)
